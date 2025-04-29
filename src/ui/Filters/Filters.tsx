40d739a8b6f923887df8fe725465effc1838bf9b
Ivan <kleninivan01@mail.ru>
2025-04-29 20:58:59 +0300

    вынес массивы в Filters в отдельные переменные

--- src/ui/Filters/Filters.tsx
+++ src/ui/Filters/Filters.tsx
@@ -3,18 +3,18 @@ import Select from "../Select/Select";
 import styles from "./Filters.module.css";
 
 const Filters = () => {
+  const sortOptions = ["Популярности", "Дате выхода", "Рейтингу"];
+  const yearOptions = ["2020", "2019", "2018"];
+  const genres = ["Комедия", "Боевик", "Драма"];
   return (
     <div className={styles.filters}>
       <h3 className={styles.header}>
         Фильтры<button className={styles.buttonClose}>X</button>
       </h3>
 
-      <Select
-        label="Сортировать по:"
-        options={["Популярности", "Дате выхода", "Рейтингу"]}
-      />
-      <Select label="Год реллиза:" options={["2020", "2019", "2018"]} />
-      <Checkboxes label="Жанры" options={["Комедия", "Боевик", "Драма"]} />
+      <Select label="Сортировать по:" options={sortOptions} />
+      <Select label="Год реллиза:" options={yearOptions} />
+      <Checkboxes label="Жанры" options={genres} />
     </div>
   );
 };

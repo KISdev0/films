e1f6ed3a263e13c0ddd8566619a75213b2649ae3
Ivan <kleninivan01@mail.ru>
2025-05-06 13:08:04 +0300

    обернул app в провайдер

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import Filters from "./ui/Filters/Filters";
 import { Header } from "./ui/Header/Header";
 import Pagination from "./ui/Pagination/Pagination";
 import "./global.css";
+import { AuthProvider } from "./AuthContext";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Filters />
-      <Pagination />
-    </div>
+    <AuthProvider>
+      <div>
+        <Header />
+        <Filters />
+        <Pagination />
+      </div>
+    </AuthProvider>
   );
 }
 
